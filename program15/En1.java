321aa5526f76206aefdbece1e686662a72301e53
ktq2390317@stu.o-hara.ac.jp <ktq2390317@stu.o-hara.ac.jp>
2024-01-26 14:54:43 +0900

    Commit

--- program15/En1.java
+++ program15/En1.java
@@ -6,19 +6,19 @@ public class En1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         try {
-        // 整数を入力する
-        System.out.print("整数1 = ");
-        int num1 = scanner.nextInt();
-        System.out.print("整数2 = ");
-        int num2 = scanner.nextInt();
-        // 少数の表示
-        int result = num1 / num2;
-        System.out.println(num1 + " / " + num2 + " = " + result);
-        // ０で割られたとき
-        } catch (ArithmeticException e) {
-            System.out.println("0による割り算です!!");
-        } finally {
-            System.out.println("処理終了");
-        }
+            // 整数を入力する
+            System.out.print("整数1 = ");
+            int num1 = scanner.nextInt();
+            System.out.print("整数2 = ");
+            int num2 = scanner.nextInt();
+            // 少数の表示
+            int result = num1 / num2;
+            System.out.println(num1 + " / " + num2 + " = " + result);
+            // ０で割られたとき
+            } catch (ArithmeticException e) {
+                System.out.println("0による割り算です!!");
+            } finally {
+                System.out.println("処理終了");
+            }
     }
 }
--- program15/En2.java
+++ program15/En2.java
@@ -7,15 +7,19 @@ public class En2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         try {
-        System.out.print("整数を入力 = ");
-        int num = scanner.nextInt();
-        if (num % 2 == 0)
-        System.out.println(num + "は偶数");
-        else
-        System.out.println(num + "は奇数");
-        } catch (InputMismatchException e) {
-            System.out.println("整数と認識できません!!");
-        }
+            // 整数を入力する
+            System.out.print("整数を入力 = ");
+            int num = scanner.nextInt();
+            // 偶数の場合
+            if (num % 2 == 0)
+            System.out.println(num + "は偶数");
+            // 奇数の場合
+            else
+            System.out.println(num + "は奇数");
+            // 実数値が入力された場合
+            } catch (InputMismatchException e) {
+                System.out.println("整数と認識できません!!");
+            }
     
     }
 
